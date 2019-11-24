1a06e7a56945c484a550e9c23a4ab4977d56c6cc
zk <2535828381@qq.com>
2019-11-24 15:13:18 +0800

    合并了冲突

--- practice/src/practice/HelloWorld.java
+++ practice/src/practice/HelloWorld.java
@@ -1,16 +1,12 @@
 package practice;
-<<<<<<< HEAD
-//aaaa
-=======
-//bbbb
->>>>>>> branch 'master' of https://github.com/zhang-sijia/eclipsegitremote.git
+//aaaa+bbbb
 public class HelloWorld {
 	public static void main(String[]args) {
 		//123456
 		//hello world
 		//123
 		//sdfgfhg
-		//haolhaol¬∫√É√Å√ã
+		//haolhaol∫√¡À
 		//789789
 		System.out.print("hello ,world");
 	}
